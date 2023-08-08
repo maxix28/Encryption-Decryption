aee3cfd3e167a127d5b2a99c169613dbb2622fd3
MaxPavliv <maxyxpavliv@gmail.com>
2023-08-08 17:20:29 +0300

    add codder and decoder

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,6 @@ fun first(input:String):String{
     }
     return result
 }
-
 val alphabet="abcdefghijklmnopqrstuvwxyz"
 fun caeser_cypher(input: String,n:Int):String{
     var result=""
@@ -24,7 +23,28 @@ fun caeser_cypher(input: String,n:Int):String{
     }
     return result
 }
+fun UNi_Coder(input: String,n:Int) :String{
+    var result=""
+    var ind=0
+    for ( a in input) {
+        result+=a.plus(n)
+    }
+    return result
+}
+fun UNi_Decoder(input: String,n:Int) :String{
+    var result=""
+    var ind=0
+    for ( a in input) {
+        result += Char(a.code - n)
+    }
+    return result
+}
+
 fun main() {
-    println(caeser_cypher(readln(), readln().toInt()))
+    when(readln()){
+        "dec"-> println( UNi_Decoder(readln(), readln().toInt()))
+        "enc"->println( UNi_Coder(readln(), readln().toInt()))
+
+    }
 
 }
