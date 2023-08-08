8fcd0b5464836c1b66e6b6a3769bc48584d0311f
MaxPavliv <maxyxpavliv@gmail.com>
2023-08-08 17:02:21 +0300

    Caeser_cypher

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,6 +8,23 @@ fun first(input:String):String{
     }
     return result
 }
+
+val alphabet="abcdefghijklmnopqrstuvwxyz"
+fun caeser_cypher(input: String,n:Int):String{
+    var result=""
+    var ind=0
+    for ( a in input){
+        if(a.isLetter()){
+            if((alphabet.indexOf(a,0,false) +n)>26){
+                ind=alphabet.indexOf(a,0,false) +n-26}
+            else { ind = alphabet.indexOf(a,0,false) +n}
+
+            result+= alphabet[ ind ]    }
+        else result +=a
+    }
+    return result
+}
 fun main() {
-    println(first("we found a treasure!"))
+    println(caeser_cypher(readln(), readln().toInt()))
+
 }
