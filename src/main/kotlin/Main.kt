269e74a50d7d0f779d735c1d9d5aefea779c685f
MaxPavliv <maxyxpavliv@gmail.com>
2023-08-08 16:42:31 +0300

    start

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+package encryptdecrypt
+fun first(input:String):String{
+    var result=""
+    for ( a in input){
+        if("[a-z]".toRegex().matches(a.toString()))
+        { result+=('z' - (a - 'a'))}
+        else result +=a
+    }
+    return result
+}
+fun main() {
+    println(first("we found a treasure!"))
 }
